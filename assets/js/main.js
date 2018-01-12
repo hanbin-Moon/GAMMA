8a73dc6bca7cd058ebe13dc94d41ccaef3348d16
hanbin-Moon <reward00@hanmail.net>
2018-01-12 18:03:07 +0900

    File upload 수정
    File upload 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -239,6 +239,18 @@ zingchart.render({
     height : 470,
     width: 680
 });
+$(document).ready(function(){
+  var fileTarget = $('.filebox .upload-hidden');
 
+    fileTarget.on('change', function(){
+        if(window.FileReader){
+            var filename = $(this)[0].files[0].name;
+        } else {
+            var filename = $(this).val().split('/').pop().split('\\').pop();
+        }
+
+        $(this).siblings('.upload-name').val(filename);
+    });
+});
 
 })(jQuery);
