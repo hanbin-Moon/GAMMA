caf44eb8f876a591e50075ceead505389a261297
wndzlf <wndzlf@gmail.com>
2018-01-16 17:37:30 +0900

    PHP변수 JS로 받아옴

--- upload_name.php
+++ upload_name.php
@@ -135,10 +135,340 @@
 			<script src="assets/js/jquery.scrollex.min.js"></script>
 			<script src="assets/js/skel.min.js"></script>
 			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+			<?php
+			$fp = fopen("/Users/admin/Desktop/P.txt","r"); //  text.txt파일을 한 줄씩 읽습니다.
+			//$arr;
+			if(!$fp) {  // $fp파일이 없으면 에러 출력
+ 			echo "error";
+			}
+			while(!feof($fp)) { //문자의 마지막 행까지 간다
+  		$str = fgets($fp,10000); // 10000길이까지 읽어드리지만 중간에 개행문자가 있으면 알아서 멈춘다.
+  		$arr[] = $str + 0; // $arr배열에 하나씩 넣는다. $b[1] = "첫번째 줄" 뭐 이런식
+			}
+			fclose($fp);
 
+			$fp2 = fopen("/Users/admin/Desktop/F.txt","r"); //  text.txt파일을 한 줄씩 읽습니다.
+			//$arr;
+			if(!$fp2) {  // $fp파일이 없으면 에러 출력
+ 			echo "error";
+			}
+			while(!feof($fp2)) { //문자의 마지막 행까지 간다
+  		$str2 = fgets($fp2,10000); // 10000길이까지 읽어드리지만 중간에 개행문자가 있으면 알아서 멈춘다.
+  		$arr2[] = $str2 + 0; // $arr배열에 하나씩 넣는다. $b[1] = "첫번째 줄" 뭐 이런식
+			}
+			fclose($fp2);
+			?>
+			<script>
+			/*
+				Transitive by TEMPLATED
+				templated.co @templatedco
+				Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+			*/
+			(function($) {
+
+				skel.breakpoints({
+					xlarge:	'(max-width: 1680px)',
+					large:	'(max-width: 1280px)',
+					medium:	'(max-width: 980px)',
+					small:	'(max-width: 736px)',
+					xsmall:	'(max-width: 480px)'
+				});
+
+				$(function() {
+
+					var	$window = $(window),
+						$body = $('body'),
+						$header = $('#header'),
+						$banner = $('#banner');
+
+					// Disable animations/transitions until the page has loaded.
+						$body.addClass('is-loading');
+
+						$window.on('load', function() {
+							window.setTimeout(function() {
+								$body.removeClass('is-loading');
+							}, 100);
+						});
+
+					// Prioritize "important" elements on medium.
+						skel.on('+medium -medium', function() {
+							$.prioritize(
+								'.important\\28 medium\\29',
+								skel.breakpoint('medium').active
+							);
+						});
+
+					// Fix: Placeholder polyfill.
+						$('form').placeholder();
+
+					// Header.
+						if (skel.vars.IEVersion < 9)
+							$header.removeClass('alt');
+
+						if ($banner.length > 0
+						&&	$header.hasClass('alt')) {
+
+							$window.on('resize', function() { $window.trigger('scroll'); });
+
+							$banner.scrollex({
+								bottom:		$header.outerHeight(),
+								terminate:	function() { $header.removeClass('alt'); },
+								enter:		function() { $header.addClass('alt'); },
+								leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
+							});
+
+						}
+
+					// Banner.
+
+						if ($banner.length > 0) {
+
+							// IE fix.
+								if (skel.vars.IEVersion < 12) {
+
+									$window.on('resize', function() {
+
+										var wh = $window.height() * 0.60,
+											bh = $banner.height();
+
+										$banner.css('height', 'auto');
+
+										window.setTimeout(function() {
+
+											if (bh < wh)
+												$banner.css('height', wh + 'px');
+
+										}, 0);
+
+									});
+
+									$window.on('load', function() {
+										$window.triggerHandler('resize');
+									});
+
+								}
+
+							// Video check.
+								var video = $banner.data('video');
+
+								if (video)
+									$window.on('load.banner', function() {
+
+										// Disable banner load event (so it doesn't fire again).
+											$window.off('load.banner');
+
+										// Append video if supported.
+											if (!skel.vars.mobile
+											&&	!skel.breakpoint('large').active
+											&&	skel.vars.IEVersion > 9)
+												$banner.append('<video autoplay loop><source src="' + video + '.mp4" type="video/mp4" /><source src="' + video + '.webm" type="video/webm" /></video>');
+
+									});
+
+							// More button.
+								$banner.find('.more')
+									.addClass('scrolly');
+
+						}
+
+					// Scrolly.
+						if ( $( ".scrolly" ).length ) {
+
+							var $height = $('#header').height() * 0.95;
+
+							$('.scrolly').scrolly({
+								offset: $height
+							});
+						}
+
+					// Menu.
+						$('#menu')
+							.append('<a href="#menu" class="close"></a>')
+							.appendTo($body)
+							.panel({
+								delay: 500,
+								hideOnClick: true,
+								hideOnSwipe: true,
+								resetScroll: true,
+								resetForms: true,
+								side: 'right'
+							});
+
+				});
+				//zingchart.THEME="classic";
+
+			var colors = {
+			    blue : "rgba(151,187,205,1)",
+			    gray : "#EBEBEB",
+			    grayDark : "#3F3F3F"
+			};
+
+
+			function randomVal(min, max, num){
+			    var aData = [];
+			    for(var i = 0 ; i < num; i++){
+			        var val = ((Math.random() * (max-min)) + min);
+			        aData.push(parseInt(val));
+			    }
+			    return aData;
+			}
+
+			var myConfig = {
+			    type : 'bar',
+			    backgroundColor : "#FFF",
+			    plot : {
+			        lineColor : "rgba(151,187,205,1)",
+			        lineWidth : "2px",
+			        backgroundColor2 : "rgba(151,187,205,1)",
+			        marker : {
+			            backgroundColor : "rgba(151,187,205,1)",
+			            borderColor : "white",
+			            shadow : false
+			        }
+			    },
+			    plotarea : {
+			        backgroundColor : "white"
+			    },
+			    scaleX : {
+			      lineColor : colors.gray,
+			      lineWidth : "1px",
+			      tick : {
+			          lineColor : "#C7C7C7",
+			          lineWidth : "1px"
+			      },
+			      guide :{
+			          lineStyle : 'solid',
+			          lineColor : colors.gray,
+			          alpha : 1
+			      },
+			      item : {
+			          color: colors.grayDark
+			      }
+			    },
+			    scaleY : {
+			      lineColor : colors.gray,
+			      lineWidth : "1px",
+			      tick : {
+			          lineColor : "#C7C7C7",
+			          lineWidth : "1px"
+			      },
+			        guide :{
+			          lineStyle : 'solid',
+			          lineColor : colors.gray,
+			          alpha : 1
+			      },
+			      item : {
+			          color: colors.grayDark
+			      }
+			    },
+			    series : [
+			        {
+			            values : randomVal(22,42,8),
+			            lineColor : "rgba(220,220,220,1)",
+			            lineWidth : "2px",
+			            alpha : 0.5,
+			            borderWidth : "2px",
+			            borderColor : "#C7C7C7",
+			            borderTop : "2px solid #C7C7C7",
+			            borderBottom : "0px",
+			            backgroundColor1 : "rgba(220,220,220,1)",
+			            backgroundColor2 : "rgba(220,220,220,1)",
+			            marker : {
+			                backgroundColor : "rgba(220,220,220,1)",
+			            }
+
+			        },
+			        {
+			            alpha : 0.5,
+			            values : randomVal(7,32,8),
+			            backgroundColor1 : colors.blue,
+			            backgroundColor2 : colors.blue,
+			            borderWidth : "2px",
+			            borderColor : colors.blue,
+			            borderTop : "2px solid " + colors.blue,
+			            borderBottom : "0px"
+			        }
+			    ]
+			}
+			// __document.ready
+			var arr = <?= json_encode($arr) ?>;
+			var arr2 =<?= json_encode($arr2) ?>;
+
+			var myChart = {
+			  "type": "bar",
+			  "title": {
+			    "text": "P Value",
+			    "fontSize": 11
+			  },
+			  "subtitle": {
+			    "text": ""
+			  },
+			  "plot": {
+			    "animation": {
+			      "delay": "100",
+			      "effect": "4",
+			      "method": "5",
+			      "sequence": "1"
+			    }
+			  },
+			  "series": [
+			    {
+			      "values": arr
+			    },
+			  ]
+			};
+			var myChart2 = {
+			  "type": "bar",
+			  "title": {
+			    "text": "F Value",
+			    "fontSize": 11
+			  },
+			  "subtitle": {
+			    "text": ""
+			  },
+			  "plot": {
+			    "animation": {
+			      "delay": "100",
+			      "effect": "4",
+			      "method": "5",
+			      "sequence": "1"
+			    }
+			  },
+			  "series": [
+			    {
+			      "values": arr2
+			    },
+			  ]
+			};
+			zingchart.render({
+			  id: "myChart",
+			  data: myChart,
+			  height: "40%",
+			  width: "100%"
+			});
+			zingchart.render({
+			  id: "myChart2",
+			  data: myChart2,
+			  height: "40%",
+			  width: "100%"
+			});
+			$(document).ready(function(){
+			  var fileTarget = $('.filebox .upload-hidden');
+
+			    fileTarget.on('change', function(){
+			        if(window.FileReader){
+			            var filename = $(this)[0].files[0].name;
+			        } else {
+			            var filename = $(this).val().split('/').pop().split('\\').pop();
+			        }
+
+			        $(this).siblings('.upload-name').val(filename);
+			    });
+			});
+			})(jQuery);
+			</script>
 <?php
-//include("config.php");
+
 $db = mysqli_connect('localhost','root','111111','gamma');
 
 if(mysqli_connect_error()){
@@ -157,11 +487,6 @@ if($result->num_rows==1)
 	exit(1);
 }(?,$uploaded_file,$_POST['value'])";
 */
-//scp 연결 부분!!
-$connection2 = ssh2_connect('GAMMA@210.94.194.55', 2222);
-ssh2_auth_password($connection2, 'GAMMA', 'gamma1234');
-ssh2_scp_send($connection2, '/Users/admin/Desktop/gammaData/wndzlf@gmail.com', '/home/GAMMA/chotest', 0644);
-
 if($_FILES['the_file']['error']>0)
 {
   echo 'Problem: ';
@@ -200,8 +525,8 @@ if($_FILES['the_file']['error']>0)
 */
 mkdir("/Users/admin/Desktop/gammaData/".$_POST['value'],0777);
 $uploaded_file='/Users/admin/Desktop/gammaData/'.$_POST['value'].'/'.$_FILES['the_file']['name'];
+$uploaded_file_copy='/Users/admin/Desktop/gammaData/'.$_POST['value'];
 $a=$_POST['value'];
-
 /*
 $find_email = "SELECT id FROM test WHERE email= '$a'";
 $find_result_email = mysql_query($db,$query);
@@ -228,7 +553,22 @@ else
   echo $_FILES['the_file']['name'];
   exit;
 }
+
+$handle = fopen("/Users/admin/Desktop/P.txt", "r");
+ if ($handle) {
+	  $i=0;
+    while (!feof($handle)) {
+        $buffer[100] = fgetss($handle, 1024);
+    }
+    fclose($handle);
+ }
+
+//scp 연결 부분!!
+$connection2 = ssh2_connect('210.94.194.55', 22);
+ssh2_auth_password($connection2, 'GAMMA', 'gamma1234');
+ssh2_scp_send($connection2, $uploaded_file_copy, '/home/GAMMA', 0644);
 echo 'File uploaded successfully.';
 ?>
+
 </body>
 </html>
